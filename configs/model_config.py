a27331f0020dd979d0b4d1f8d37cbc43e804f6e7
stephenzzf <136873417+stephenzzf@users.noreply.github.com>
2023-06-30 20:47:21 +0800

    Update model_config.py

--- configs/model_config.py
+++ configs/model_config.py
@@ -111,7 +111,7 @@ llm_model_dict = {
 }
 
 # LLM 名称
-LLM_MODEL = "chatglm-6b"
+LLM_MODEL = "chatglm-6b-int4-qe"
 # 量化加载8bit 模型
 LOAD_IN_8BIT = False
 # Load the model with bfloat16 precision. Requires NVIDIA Ampere GPU.
